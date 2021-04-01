b4da798e7c69e2b5f3a42babd88e51c493a2db0e
Garbo alan <alan-garbo@hotmail.fr>
2021-04-01 17:32:08 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import json
+
+apikey = 'SoQ0KKGCnHIPmbF7Mz9EZnSjrVjREBTYV0IWXnp-OrXp'
+url = 'https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/09a40c2d-2a13-4f01-ad42-9b634c374daa'
+
+authenticator = IAMAuthenticator(apikey)
+tts = TextToSpeechV1(authenticator=authenticator)
+tts.set_service_url(url)
+
+voices = tts.list_voices().get_result()
+print(json.dumps(voices, indent=2))
+
+with open('./speech.mp3', 'wb') as audio_file:
+    res = tts.synthesize('Hello World! Its Marseille baby', accept='audio/mp3', voice='en-US_AllisonV3Voice').get_result()
+    audio_file.write(res.content)
+
+with open('drake.txt', 'r') as f:
+    text = f.readlines()
+    text = [line.replace('\n','') for line in text]
+    text = ''.join(str(line) for line in text)
+    
+with open('./drake.mp3', 'wb') as audio_file:
+    res = tts.synthesize(text, accept='audio/mp3', voice='en-GB_JamesV3Voice').get_result()
+    audio_file.write(res.content)
+
+with open ("freeze.txt", 'r') as f:
+    text = f.readlines()
+    text = [line.replace('\n','') for line in text]
+    text = ''.join(str(line) for line in text)
+
+voice = 'fr-FR_ReneeVoice'
+
+with open('./freeze.mp3', 'wb') as audio_file:
+    res = tts.synthesize(text, accept='audio/mp3', voice=voice).get_result()
+    audio_file.write(res.content)
